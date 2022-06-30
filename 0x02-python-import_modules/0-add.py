df9483efe7a3756aa7c4fa9de6b4e53e337e47c6
Caren-Koroeny <carensiya5@gmail.com>
2022-06-30 02:17:14 -0700

    adds two numbers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+author : Caren Siya
+
+if __name__ == "__main__":
+	"""Print the sum of <a variable> and <b variable>."""
+	from add_0 import add
+
+	a = 1
+	b = 2
+	print("{} + {}" = {}".format(a, b, add (a,b)))
