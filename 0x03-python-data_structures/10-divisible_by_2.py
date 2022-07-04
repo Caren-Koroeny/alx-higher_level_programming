f6058b229d369158f1fbf5b5d65eee02b1f52e84
Caren-Koroeny <carensiya5@gmail.com>
2022-07-04 03:17:51 -0700

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
@@ -6,4 +7,4 @@ def divisible_by_2(my_list=[]):
             new_list.append(True)
         else:
             new_list.append(False)
-    return 
+    return new_list
